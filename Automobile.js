6bf4c17d75a3b79632d3a69fc81bcd99530d5514
Megan Aldridge <moogalin@users.noreply.github.com>
2016-04-24 00:53:54 -0500

    Added files via upload

--- Automobile.js
+++ Automobile.js
@@ -1,10 +1,21 @@
 
-function Automobile( year, make, model, type ){
+function Automobile( year, make, model, type){
     this.year = year; //integer (ex. 2001, 1995)
     this.make = make; //string (ex. Honda, Ford)
     this.model = model; //string (ex. Accord, Focus)
     this.type = type; //string (ex. Pickup, SUV)
 }
+
+Automobile.prototype.logMe = function(status) {
+    if (status == 'true') {
+      //print year, make, model
+      console.log(this.year + " " + this.make + " " + this.model + " " + this.type);
+    }
+    else if (status == 'false') {
+      //print year, make, model, and type
+      console.log(this.year + " " + this.make + " " + this.model);
+    }
+};
 
 var automobiles = [
     new Automobile(1995, "Honda", "Accord", "Sedan"),
@@ -15,11 +26,27 @@ var automobiles = [
     new Automobile(2008, "Subaru", "Outback", "Wagon")
     ];
 
+var numbersArr = [1, 4, 7, 3, 6];
+
 /*This function sorts arrays using an arbitrary comparator. You pass it a comparator and an array of objects appropriate for that comparator and it will return a new array which is sorted with the largest object in index 0 and the smallest in the last index*/
 function sortArr( comparator, array ){
     /*your code here*/
-    var arr = [1, 2, 3, 4, 5]
-    return arr;
+    console.log("\nBefore sort:");
+
+    var funcArray = array;
+
+    for (var i = 0; i < array.length; i++) {
+      console.log(funcArray[i]);
+    }
+
+
+
+    console.log("\nAfter sort:");
+
+    for (var k = 0; k < array.length; k++) {
+      console.log(funcArray[k]);
+    }
+    return funcArray;
 }
 
 /*A comparator takes two arguments and uses some algorithm to compare them. If the first argument is larger or greater than the 2nd it returns true, otherwise it returns false. Here is an example that works on integers*/
@@ -36,17 +63,68 @@ function exComparator( int1, int2){
 /*This compares two automobiles based on their year. Newer cars are "greater" than older cars.*/
 function yearComparator( auto1, auto2){
     /* your code here*/
+    if (auto1.year > auto2.year) {
+      return true;
+    }
+    else {
+      return false;
+    }
+
 }
 
 /*This compares two automobiles based on their make. It should be case insensitive and makes which are alphabetically earlier in the alphabet are "greater" than ones that come later.*/
 function makeComparator( auto1, auto2){
     /* your code here*/
+    var one = auto1.make.toUpperCase();
+    var two = auto2.make.toUpperCase();
+
+    if (auto1.make < auto2.make) {
+      return true;
+    }
+    else {
+      return false;
+    }
+
 }
 
 /*This compares two automobiles based on their type. The ordering from "greatest" to "least" is as follows: roadster, pickup, suv, wagon, (types not otherwise listed). It should be case insensitive. If two cars are of equal type then the newest one by model year should be considered "greater".*/
 function typeComparator( auto1, auto2){
     /* your code here*/
-}
+    var one = auto1.type.toUpperCase();
+    var two = auto1.type.toUpperCase();
+
+    var rankType = ["WAGON", "SUV", "PICKUP", "ROADSTER"];
+
+    console.log("index of ROADSTER: " + rankType.indexOf("ROADSTER"));
+    console.log("index of PICKUP: " + rankType.indexOf("PICKUP"));
+    console.log("index of ROADSTER: " + rankType.indexOf("ROADSTER"));
+    console.log("index of SUV: " + rankType.indexOf("SUV"));
+    console.log("index of WAGON: " + rankType.indexOf("WAGON"));
+    console.log("index of SEDAN: " + rankType.indexOf("SEDAN"));
+
+    console.log("First comparison:" + auto1.type + " vs. " + auto2.type);
+    if (rankType.indexOf(auto1.type) > rankType.indexOf(auto2.type)) {
+      console.log(auto1.type + " greater than " + auto2.type);
+      return true;
+    }
+    else if ( rankType.indexOf(auto1.type) == rankType.indexOf(auto2.type) ) {
+      console.log("auto types are equal");
+      if (auto1.year > auto2.year) {
+        console.log(auto1.year + " greater than " + auto2.year);
+        return true;
+      }
+      else {
+        console.log(auto1.year + " less than " + auto2.year);
+        return false;
+      }
+    }
+
+    else {
+      console.log(auto1.type + " less than " + auto2.type);
+      return false;
+    }
+
+  }
 
 
 /*Your program should output the following to the console.log, including the opening and closing 5 stars. All values in parenthesis should be replaced with appropriate values. Each line is a seperate call to console.log.
@@ -55,34 +133,58 @@ Each line representing a car should be produced via a logMe function. This funct
 */
 
 console.log('*****');
+console.log('\nExample:');
+
+sortArr(exComparator, numbersArr);
+
+console.log("\n" + yearComparator(automobiles[0], automobiles[1]));
+console.log(yearComparator(automobiles[1], automobiles[2]));
+console.log(yearComparator(automobiles[2], automobiles[3]));
+console.log(yearComparator(automobiles[3], automobiles[4]));
+console.log(yearComparator(automobiles[4], automobiles[5]));
+
+console.log("\n" + makeComparator(automobiles[0], automobiles[1]));
+console.log(makeComparator(automobiles[1], automobiles[2]));
+console.log(makeComparator(automobiles[2], automobiles[3]));
+console.log(makeComparator(automobiles[3], automobiles[4]));
+console.log(makeComparator(automobiles[4], automobiles[5]));
+
+console.log("\n" + typeComparator(automobiles[0], automobiles[1]));
+console.log(typeComparator(automobiles[1], automobiles[2]));
+console.log(typeComparator(automobiles[2], automobiles[3]));
+console.log(typeComparator(automobiles[3], automobiles[4]));
+console.log(typeComparator(automobiles[4], automobiles[5]));
+
+
 console.log('The cars sorted by year are:');
 /*(year make model of the 'greatest' car)
 (...)
 */
-for (var i= 0; i < sortArr().length; i++) {
-  console.log(sortArr()[i]);
+for (var i = 0; i < automobiles.length; i++) {
+  automobiles[i].logMe('false');
 }
+
 /*
 (year make model of the 'least' car)
 */
 
-console.log('The cars sorted by make are:');
+console.log('\nThe cars sorted by make are:');
 /*(year make model of the 'greatest' car)
 (...)
 */
-for (var i= 0; i < sortArr().length; i++) {
-  console.log(sortArr()[i]);
+for (var i = 0; i < automobiles.length; i++) {
+  automobiles[i].logMe('false');
 }
 /*
 (year make model of the 'least' car)
 */
 
-console.log('The cars sorted by type are:');
+console.log('\nThe cars sorted by type are:');
 /*(year make model type of the 'greatest' car)
 (...)
 */
-for (var i= 0; i < sortArr().length; i++) {
-  console.log(sortArr()[i]);
+for (var i = 0; i < automobiles.length; i++) {
+  automobiles[i].logMe('true');
 }
 /*
 (year make model type of the 'least' car)
